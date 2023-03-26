5d9a9b70c4a5d78721980e9a236943bbf29e4eb7
Anatol <anatolij.trush@yandex.ru>
2023-03-26 14:11:21 +0300

    TODO: (time)

--- mainwindow.h
+++ mainwindow.h
@@ -88,5 +88,6 @@ private:
     void playCanFile();  
 };
 #endif // MAINWINDOW_H
+// TODO: ?Time spent/time left
 // TODO: ?Calc spent dist + Supports + Make objs
 // TODO: ?Redo visImg, panels (add ID) + write log + color legend for regions
